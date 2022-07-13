b730ad409e03d033d1e3d0d67132b304499a5af2
Peter Cameron <petercameron@users.noreply.github.com>
2022-07-13 18:06:08 +0100

    make compatible with MySQL OUTFILE

--- macros/plugins/snowflake/create_external_table.sql
+++ macros/plugins/snowflake/create_external_table.sql
@@ -18,7 +18,7 @@
             {%- set column_quoted = adapter.quote(column.name) if column.quote else column.name %}
             {%- set col_expression -%}
                 {%- set col_id = 'value:c' ~ loop.index if is_csv else 'value:' ~ column_quoted -%}
-                (case when is_null_value({{col_id}}) or lower({{col_id}}) = 'null' then null else {{col_id}} end)
+                (case when is_null_value({{col_id}}) or lower({{col_id}}) = '\\n' then null else {{col_id}} end)
             {%- endset %}
             {{column_quoted}} {{column.data_type}} as ({{col_expression}}::{{column.data_type}})
             {{- ',' if not loop.last -}}
